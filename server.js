aaab240e10574fc5a78953972a7f50242494930a
Jofemago <jofemago@utp.edu.co>
2019-01-24 01:51:10 -0500

    se crea api que lanza servidores dentro de la misma maquina

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const shell = require("shelljs");
+var sleep = require("sleep");
 // App instand
 const app = express();
 
@@ -21,6 +23,61 @@ app.get("/status", (req, res) => {
 
 app.get("/all-servers", (req, res) => {
   res.send(listServers);
+});
+
+app.get("/startServer", (req, res) => {
+  if (req.query.host !== undefined && req.query.port !== undefined) {
+    const port = req.query.port;
+    const host = req.query.host;
+    const name = req.query.name;
+    const memory = req.query.memory;
+    const cpu = req.query.cpu;
+    //res.send({ puerto: puerto, host: host });
+
+    const data = {
+      host: req.query.host,
+      port: req.query.port,
+      name: req.query.name,
+      memory: req.query.memory,
+      cpu: req.query.cpu
+    };
+
+    const holis = shell.exec(`cd server &&  PORT=${port} npm -d start &`, {
+      async: true
+    });
+
+    sleep.sleep(5);
+    //editar para validar
+    let state = false;
+    listServers.forEach(server => {
+      state = state
+        ? state
+        : data.host == server.host && data.port == server.port;
+    });
+    if (!state) {
+      let options = {
+        url: "http://" + data.host + ":" + data.port + "/status",
+        method: "GET",
+        jar: true
+      };
+
+      request(options, (err, resp, body) => {
+        if (!err && resp.statusCode == 200) {
+          listServers.push(data);
+          res.send({ success: true, err: null, msg: "Added" });
+        } else {
+          res.send({ success: true, err: null, msg: "Server not found" });
+        }
+      });
+    } else
+      res.send({
+        success: true,
+        err: null,
+        msg: "Server already added"
+      });
+
+    //res.send({ status: true, out: holis });
+  } else res.send({ success: false });
 });
 
 //en la ventana en la que se ingresan los requerimientos  la funcion que actualiza la pagina se consume
@@ -72,6 +129,6 @@ app.get("/add", (req, res) => {
     res.send({ success: false, err: true, msg: "Especifique puerto y host" });
 });
 
-app.listen("8081", () => {
+app.listen("8083", () => {
   console.log("Server is running...");
 });
